50af8db22793ae0442c47785620aa0029a907136
StefanosTouf <stefetoufe@gmail.com>
2022-04-03 15:06:48 +0300

    add toUUID util

--- app/src/main/java/com/steft/travel_app/common/Utils.kt
+++ app/src/main/java/com/steft/travel_app/common/Utils.kt
@@ -1,12 +1,15 @@
 package com.steft.travel_app.common
 
-import arrow.core.Either
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import java.util.*
 
 object Utils {
     suspend fun <A, B> Iterable<A>.pMap(f: suspend (A) -> B): List<B> = coroutineScope {
         map { async { f(it) } }.awaitAll()
     }
+
+    fun String.toUUID(): UUID =
+        UUID.fromString(this)
 }
