7d7073efc4d0dc6c8eb1132a8f076a8b3e9ea730
StefanosTouf <stefetoufe@gmail.com>
2022-04-25 22:07:09 +0300

    add example bundles fragment

--- app/src/main/java/com/steft/travel_app/view/Bundles.kt
+++ app/src/main/java/com/steft/travel_app/view/Bundles.kt
@@ -5,56 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.steft.travel_app.R
+import com.steft.travel_app.placeholder.PlaceholderContent
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Bundles.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Bundles : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val items = arrayListOf(
+        PlaceholderContent.PlaceholderItem("asd", "dasdawdwa", "details"),
+        PlaceholderContent.PlaceholderItem("asd", "12323asdsad", "details"),
+        PlaceholderContent.PlaceholderItem("asd", "aasdassd", "details")
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bundles, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_bundles, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Bundles.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Bundles().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = LinearLayoutManager(context)
+
+                viewLifecycleOwner.lifecycleScope.launch {
+                    adapter = MyItemRecyclerViewAdapter(items)
                 }
             }
+        }
+        return view
     }
 }
