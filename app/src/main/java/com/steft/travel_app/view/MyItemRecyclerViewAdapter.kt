497b0d6b117c5fa4b565c4f5247a71814612a838
Rita <ritakialex@hotmail.com>
2022-05-09 22:50:52 +0300

    changed MyItemRecyclerViewAdapter to not throw errors
    for the moment

--- app/src/main/java/com/steft/travel_app/view/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/com/steft/travel_app/view/MyItemRecyclerViewAdapter.kt
@@ -1,24 +1,37 @@
 package com.steft.travel_app.view
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.steft.travel_app.databinding.FragmentLocationsBinding
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.Shapeable
+import com.steft.travel_app.R
+//import com.steft.travel_app.databinding.FragmentLocationsBinding
 import com.steft.travel_app.placeholder.PlaceholderContent.PlaceholderItem
 
 
 class MyItemRecyclerViewAdapter(private val values: ArrayList<PlaceholderItem>) :
     RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder>() {
 
-    inner class ViewHolder(binding: FragmentLocationsBinding) :
+/*    inner class ViewHolder(binding: FragmentLocationsBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         val idView: TextView = binding.itemNumber
         val contentView: TextView = binding.content
+    }*/
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        //titleImage
+        val itemImage : ShapeableImageView = itemView.findViewById(R.id.item_image)
+        val itemTitle : TextView = itemView.findViewById(R.id.item_title)
+        val itemDesc : TextView = itemView.findViewById(R.id.item_description)
+
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    /*override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             FragmentLocationsBinding.inflate(
                 LayoutInflater.from(parent.context),
@@ -26,13 +39,28 @@ class MyItemRecyclerViewAdapter(private val values: ArrayList<PlaceholderItem>) 
                 false
             )
         )
+    }*/
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false)
+
+        return ViewHolder(itemView)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    /*override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
         holder.idView.text = item.id
         holder.contentView.text = item.content
+    }*/
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = values[position]
+
+        /*
+        holder.itemImage.setImageResource(currentItem.itemImage)
+        holder.itemTitle.text = currentItem.header
+        */
     }
+
 
     override fun getItemCount(): Int = values.size
 
