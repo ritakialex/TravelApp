fe2b3bcffee17b5d15b29462dbae707422c3a9e7
Rita <ritakialex@hotmail.com>
2022-05-18 15:57:31 +0300

    logoutButton goto MainActivity

--- app/src/main/java/com/steft/travel_app/view/AgentProfile.kt
+++ app/src/main/java/com/steft/travel_app/view/AgentProfile.kt
@@ -1,11 +1,17 @@
 package com.steft.travel_app.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.steft.travel_app.R
+import com.steft.travel_app.databinding.FragmentAgentLoginBinding
+import com.steft.travel_app.databinding.FragmentAgentProfileBinding
 
 
 class AgentProfile : Fragment() {
@@ -16,9 +22,22 @@ class AgentProfile : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
+        //αν πατήσει login σε νέο activity
+        val bind = FragmentAgentProfileBinding.inflate(layoutInflater)
+
+        bind.profileLogoutButton.setOnClickListener {
+            val intent = Intent(this@AgentProfile.requireContext(),MainActivity::class.java)
+            startActivity(intent)
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agent_profile, container, false)
+        return bind.root //return inflater.inflate(R.layout.fragment_agent_profile, container, false)
     }
+
+
+
 
 
 }
