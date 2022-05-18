063498c8d3151b35eeac733085c2dd35206b59c7
Rita <ritakialex@hotmail.com>
2022-05-18 21:03:18 +0300

    added RecyclerView on Bookings
    no functionality yet

--- app/src/main/java/com/steft/travel_app/view/Bookings.kt
+++ app/src/main/java/com/steft/travel_app/view/Bookings.kt
@@ -5,20 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.steft.travel_app.R
+import com.steft.travel_app.databinding.FragmentBookingsBinding
+import com.steft.travel_app.databinding.FragmentLocationsListBinding
+import com.steft.travel_app.viewmodel.MainViewModel
+import com.steft.travel_app.viewmodel.MainViewModelFactory
+import java.util.*
 
 
 class Bookings : Fragment() {
 
+    private val viewModel by activityViewModels<MainViewModel> {
+        MainViewModelFactory(
+            requireActivity().application,
+            true,
+            UUID.fromString("df34b46c-5268-44f7-b213-c5a237447c3d"))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_bookings, container, false)
+        val bind = FragmentBookingsBinding.inflate(layoutInflater)
+        val recyclerView = bind.recyclerBookingsList
 
-        return view
+        /*try {
+            viewModel
+                .getBookings()
+                .observe(viewLifecycleOwner) { bookings ->
+                    with(recyclerView) {
+                        layoutManager = LinearLayoutManager(context)
+                        adapter = MyItemRecyclerViewAdapter(ArrayList(bookings))
+                    }
+                }
+        } catch (ex: Exception) {
+            //Do something
+            println(ex.message)
+        }*/
+
+
+        return bind.root
     }
 
 
