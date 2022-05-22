685b1dfd5a3ab97c3f42e512df57a0c2dccd0d29
Rita <ritakialex@hotmail.com>
2022-05-22 12:22:31 +0300

    Bundle shows bundle info
    needs to be un editable if user is not logged in

--- app/src/main/java/com/steft/travel_app/view/Bundle.kt
+++ app/src/main/java/com/steft/travel_app/view/Bundle.kt
@@ -33,34 +33,35 @@ class Bundle : Fragment() {
         val args = this.arguments
 
         //show bundle
-        /*try {
+        try {
             val bundleId =
                 args?.getString("bundleId")
                     ?.let { UUID.fromString(it) }
                     ?: throw IllegalStateException("Bundle id should exist")
+
 
             viewModel
                 .getBundle(bundleId)
                 .observe(viewLifecycleOwner) {
                     if (it != null) {
                         val (_, agencyId, locationId, date, price, duration, hotels, type) = it
-                        bind.dateFromBundleTextView.text = date.toString()
-                        bind.durationBundleTextView.text = duration.toString()
-                        bind.typeBundleTextView.text = type
-                        bind.priceBundleTextView.text = price.toString()
-                        bind.agencyBundleTextView.text = agencyId.toString()
+                        bind.dateFromBundleTextView.setText(date.toString())
+                        bind.durationFromBundleTextView.setText(duration.toString())
+                        bind.typeBundleTextView.setText(type)
+                        bind.priceBundleTextView.setText(price.toString())
+                        bind.agencyBundleTextView.setText(agencyId.toString())
                         //TODO hotels List
-                        bind.hotelsBundleTextView.text =
-                            hotels.tail()
-                                .fold(hotels.first()) { acc, next -> "$acc\n$next" }
+                        bind.hotelsBundleTextView.setText(hotels.tail()
+                            .fold(hotels.first()) { acc, next -> "$acc\n$next" })
+
 
                         viewModel.getLocation(locationId).observe(viewLifecycleOwner) { location ->
                             val location = location
                                 ?: throw IllegalStateException("Location id should exist")
 
                             val (_, _, city, country) = location
-                            bind.cityBundleTextView.text = city
-                            bind.countryBundleTextView.text = country
+                            bind.locationBundleInfo.setText("Bundle for $city, $country")
+                            //bind.countryBundleTextView.text = country
                         }
                     } else {
                         throw IllegalStateException("Bundle id cant be null")
@@ -69,7 +70,7 @@ class Bundle : Fragment() {
         } catch (ex: Exception) {
             Toast.makeText(context, "Something went wrong, try again", Toast.LENGTH_LONG).show()
             println(ex.message)
-        }*/
+        }
 
 
         //Book Button Traveler
